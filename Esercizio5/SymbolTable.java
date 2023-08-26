c3486e55821ff3b24e36f32181f9a0aeae1c9d3d
Jose Santos L <josesantosl1607@gmail.com>
2023-08-26 23:11:43 +0200

    fatto tutto meno il conditional

--- Esercizio5/SymbolTable.java
+++ Esercizio5/SymbolTable.java
@@ -2,19 +2,25 @@ import java.util.*;
 
 public class SymbolTable {
 
-     Map <String, Integer> OffsetMap = new HashMap <String,Integer>();
+    Map <String, Integer> OffsetMap = new HashMap <String,Integer>();
 
-	public void insert( String s, int address ) {
-            if( !OffsetMap.containsValue(address) ) 
-                OffsetMap.put(s,address);
-            else 
-                throw new IllegalArgumentException("Reference to a memory location already occupied by another variable");
-	}
+    public void insert( String s, int address ) {
+	if( !OffsetMap.containsValue(address) ) 
+	    OffsetMap.put(s,address);
+	else 
+	    throw new IllegalArgumentException("Reference to a memory location already occupied by another variable");
+    }
 
-	public int lookupAddress ( String s ) {
-            if( OffsetMap.containsKey(s) ) 
-                return OffsetMap.get(s);
-            else
-                return -1;
-	}
+    /*Voglio modificare questa funzione per passare un token invece di una
+      stringa. So non c'e bisogno ma penso che sarebbe meglio evitare la
+      repetizione nel translator, anche questo lo fa molto piu' facile da leggere
+      il Translator. Penso che non è un cambio grande nel codice e spero non sia
+      un problmea al momento dell'interrogazione.
+    */
+    public int lookupAddress ( Token t ) {
+	if( OffsetMap.containsKey(((Word)t).lexeme))
+	    return OffsetMap.get(((Word)t).lexeme);
+	else
+	    return -1;
+    }
 }
--- Esercizio5/Translator.java
+++ Esercizio5/Translator.java
@@ -1,138 +1,257 @@
 import java.io.*;
 
-public class Translator {
+public class Translator { // Un Parser32 adattato.
     private Lexer lex;
     private BufferedReader pbr;
     private Token look;
-    
-    SymbolTable st = new SymbolTable();
+
+    SymbolTable st     = new SymbolTable();
     CodeGenerator code = new CodeGenerator();
-    int count=0;
+    int count          = 0;//counter di registri del symboltable
 
     public Translator(Lexer l, BufferedReader br) {
         lex = l;
         pbr = br;
-        move();
+	move();
     }
 
-    void move() { 
-	// come in Esercizio 3.1
+    void move() {
 	look = lex.lexical_scan(pbr);
-        System.out.print(look+ " ");
 
+	if(look.tag == ';')
+	    System.out.println(look);
+	else
+	    System.out.print(look+ " ");
     }
 
-    void error(String s) { 
-	// come in Esercizio 3.1
-	throw new Error("near line " + lex.line + ": " + s);
+    private void error(String s) {
+	throw new Error("near line " + lex.line + ":"+ look +": " + s);
     }
 
-    void match(int t) {
-	// come in Esercizio 3.1
+    private void match(int t) {
 	if (look.tag == t) {
 	    if (look.tag != Tag.EOF) move();
 	} else error("syntax error");
     }
 
-    public void prog() {        
-	// ... completare ...
-        int lnext_prog = code.newLabel();
-        //statlist(lnext_prog); //ancora non capisco perche c'e' lnext_prog
-        code.emitLabel(lnext_prog);
-        match(Tag.EOF);
+    private void prog(){//prog() -> statlist(lnext) EOF
+
+	if (look.tag == Tag.EOF) {
+	    error("file vuoto");
+	}
+
+        int proglabel = code.newLabel();
+	statlist(proglabel);
+        code.emitLabel(proglabel);
+
+	match(Tag.EOF);
+
         try {
-        	code.toJasmin();
-        }
-        catch(java.io.IOException e) {
-        	System.out.println("IO error\n");
-        };
-	// ... completare ...
+	    code.toJasmin();
+        }catch(java.io.IOException e) {
+	    System.err.println("Error writting the Output.j");
+	}
+	
     }
 
-    public void stat( /* completare */ ) {
-        switch(look.tag) {
+    private void statlist(int lnext){
+	stat(lnext);
+	statlistp(lnext);
+    }
+
+    private void statlistp(int lnext){
+	if(look.tag == ';'){
+	    match(';');
+	    stat(lnext);
+	    statlistp(lnext);
+	}
+    }
+
+    private void stat(int lnext){
+	switch (look.tag) {
 	case Tag.ASSIGN:
 	    match(Tag.ASSIGN);
 	    expr();
 	    match(Tag.TO);
-	    idlist();
+	    idlist(Tag.ASSIGN);
 	    break;
 	case Tag.PRINT:
 	    match(Tag.PRINT);
 	    match('[');
-	    exprlist();
+	    exprlist(Tag.PRINT);
 	    match(']');
 	    break;
 	case Tag.READ:
 	    match(Tag.READ);
 	    match('[');
-	    idlist(/* completare */);
+	    idlist(Tag.READ);
 	    match(']');
 	    break;
 	case Tag.WHILE:
 	    match(Tag.WHILE);
-	    
+	    int whilelabel = code.newLabel();
+	    int endwhile   = code.newLabel();
+	    match('(');
+	    code.emitLabel(whilelabel);
+	    bexpr(endwhile);
+	    match(')');
+	    stat(lnext);
+	    code.emit(OpCode.GOto,whilelabel);
+	    code.emitLabel(endwhile);
+	    break;
 	case Tag.COND:
+	    match(Tag.COND);
+	    match('[');
+	    optlist();
+	    match(']');
+	    switch (look.tag) {
+	    case Tag.ELSE:
+		match(Tag.ELSE);
+		stat(lnext);
+	    case Tag.END:
+		match(Tag.END);
+		break;
+	    default:
+		error("not valid command");
+
+	    }
+	    break;
 	case '{':
+	    match('{');
+	    statlist(lnext);
+	    match('}');
+	    break;
 	default:
 	    error("not valid stat");
-        }
-     }
-    void statlist(){
-	stat();
-	statlistp();
+	}
     }
+    private void  idlist(int op){
+	if(look.tag == Tag.ID){
+	    int address = st.lookupAddress(look);
+	    if (address == -1) {
+		st.insert(((Word)look).lexeme,count++);
+		address = count;
+	    }
 
-    void statlistp(){
-	if(look.tag == ';'){
-	    match(';');
-	    stat();
-	    statlistp();
+	    if (op == Tag.READ) {
+		code.emit(OpCode.invokestatic,0); // invokestatic 0 = read
+	    }
+	    code.emit(OpCode.istore,address);
+	    match(Tag.ID);
+	    idlistp(op);
+	}else{
+	    error("no identifier was found.");
+	}
+    }
+    private void idlistp(int op){
+	if(look.tag == ','){
+	    match(',');
+	    if(look.tag == Tag.ID){
+		int address = st.lookupAddress(look);
+		if (address == -1) {
+		    st.insert(((Word)look).lexeme,count++);
+		    address = count;
+		}
+
+		if (op == Tag.READ) {
+		    code.emit(OpCode.invokestatic,0); // invokestatic 0 = read
+		}
+		code.emit(OpCode.istore,address);
+		match(Tag.ID);
+		idlistp(op);
+	    }else{
+		error("no identifier was found.");
+	    }
+
 	}
     }
 
-    private void idlist(int value) {
-        switch(look.tag) {
-	    case Tag.ID:
-        	int id_addr = st.lookupAddress(((Word)look).lexeme);
-                if (id_addr==-1) {
-                    id_addr = count;
-                    st.insert(((Word)look).lexeme,count++);
-                }
-                match(Tag.ID);
-		if(look.tag == ','){
-		    match(',');
-		    idlist(value);
-		}
-		break;
-	    default:
-		error("expected another var");
-		
-    	}
+    private void optlist(){
+	optitem();
+	optlistp();
+    }
+    private void optlistp(){
+	if(look.tag == Tag.OPTION){
+	    optitem();
+	    optlistp();
+	}
     }
 
-    private void expr() {
-        switch(look.tag) {
+    private void optitem(){
+	match(Tag.OPTION);
+	match('(');
+	//bexpr();
+	match(')');
+	match(Tag.DO);
+	//stat(lnex); //da riparare come pasare tutti i label
+    }
+
+    private void bexpr(int truelabel){
+	switch (look.tag) {
+	case Tag.RELOP:
+	    String relatinaloperator = ((Word)look).lexeme;
+	    match(Tag.RELOP);
+	    expr();
+	    expr();
+	    switch (relatinaloperator) {
+	    case ">":
+		code.emit(OpCode.if_icmpgt,truelabel);
+		break;
+	    case "<":
+		code.emit(OpCode.if_icmplt,truelabel);
+		break;
+		
+	    case "<=":
+		code.emit(OpCode.if_icmple,truelabel);
+		break;
+	    case ">=":
+		code.emit(OpCode.if_icmpge,truelabel);
+		break;
+	    case "==":
+		code.emit(OpCode.if_icmpeq,truelabel);
+		break;
+	    case "<>"
+		code.emit(OpCode.if_icmpne,truelabel);
+		break;
+	    }
+
+	    break;
+	case '!':
+	    match('!');
+	    expr();
+	    code.emit(OpCode.ineg, truelabel);
+	    break;
+	case Tag.AND:
+	    match(Tag.AND);
+	    expr();
+	    expr();
+	    code.emit(OpCode.iand, truelabel);
+	    break;
+	case Tag.OR:
+	    match(Tag.OR);
+	    expr();
+	    expr();
+	    code.emit(OpCode.ior, truelabel);
+	    break;
+	default:
+	    error("is not a relational operator");
+	}
+    }
+
+    private void expr(){
+	switch (look.tag) {
 	case '+':
-	    match('+');
+	case '*':
+	    match(look.tag);
 	    match('(');
-	    exprlist();
+	    exprlist(look.tag);
 	    match(')');
-	    code.emit(OpCode.iadd);
 	    break;
 	case '-':
 	    match('-');
 	    expr();
 	    expr();
 	    code.emit(OpCode.isub);
-	    break;
-	case '*':
-	    match('*');
-	    match('(');
-	    exprlist();
-	    match(')');
-	    code.emit(OpCode.imul);
-	    break;
 	case '/':
 	    match('/');
 	    expr();
@@ -140,41 +259,71 @@ public class Translator {
 	    code.emit(OpCode.idiv);
 	    break;
 	case Tag.NUM:
-	    code.emit(OpCode.ldc,((NumberTok)look).num);
+	    /*devo fare il cast a NumberTok perche java non sa che il look e'
+	     *un NumberTok e non un semplice Token.
+	    */
+	    int num = ((NumberTok)look).num;
+	    code.emit(OpCode.ldc,num);
 	    match(Tag.NUM);
-	    break;
 	case Tag.ID:
-	    code.emit(OpCode.iload,((Word)look).lexeme);
+	    /*Questo e' l'unico momento di lettura dei SymbolTable*/
+	    int address = st.lookupAddress(look);
+
+	    if (address != -1) {
+		code.emit(OpCode.iload, address);
+	    }else{
+		error("identifier not found.");
+	    }
 	    match(Tag.ID);
 	    break;
 	default:
-	    error("Invalid Expression");
-        }
+	    error("Invalid Expresion");
+	}
+
     }
 
-    void exprlist(){
+    private void exprlist(int op){
 	expr();
-	exprlistp();
+	if(op == Tag.PRINT){
+	    /*invokestatic 1 significa print, altrimente sarebbe READ(non e'
+	    specificato nel progetto ma possiamo dire che sarebbe 0 in quel caso)*/
+	    code.emit(OpCode.invokestatic,1);//nel caso del print dobbiamo usarlo per ogni espressione perche e' un'operazione unitaria e non binaria 
+	}
+	exprlistp(op);
     }
-    void exprlistp(){
+
+    private void exprlistp(int op){
 	if(look.tag == ','){
 	    match(',');
 	    expr();
-	    exprlistp();
+	    switch (op) {
+	    case '+':
+		code.emit(OpCode.iadd);
+		break;
+	    case '*':
+		code.emit(OpCode.imul);
+		break;
+	    case Tag.PRINT:
+		code.emit(OpCode.invokestatic,1); // invokestatic 1 = print
+		break;
+	    default:
+		error("Invalid operator ");
+		
+	    }
+	    exprlistp(op);
 	}
     }
 
-// ... completare ...
     public static void main(String[] args) {
+
 	Lexer lex = new Lexer();
-	String path = "Test.lft";
+	String path = "test.lft";
 	try {
 	    BufferedReader br = new BufferedReader(new FileReader(path));
-	    Translator translator = new Translator(lex,br);
-	    translator.prog();
+	    Translator parser = new Translator(lex, br);
+	    parser.prog();
+	    System.out.println(path+" tradotto correttamente.");
 	    br.close();
-	}
-	catch (IOException e) {e.printStackTrace();}
-
+	} catch (IOException e) {e.printStackTrace();}
     }
 }
