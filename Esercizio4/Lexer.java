a671b85bcf0f154e4a5a99ccb56129b5e716f5af
Jose Santos L <josesantosl1607@gmail.com>
2023-08-30 15:50:50 +0200

    adjust indent

--- Esercizio4/Lexer.java
+++ Esercizio4/Lexer.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Lexer {
 
-     static int line = 1;
+    static int line = 1;
     private char peek = ' ';
     
     private void readch(BufferedReader br) {
@@ -16,188 +16,186 @@ public class Lexer {
 
     public Token lexical_scan(BufferedReader br) {
         while (peek == ' ' || peek == '\t' || peek == '\n'  || peek == '\r') {
-	    if (peek == '\n'){//salto di linea
-		System.out.println();
-		line++;
-	    }
+            if (peek == '\n'){//salto di linea
+                System.out.println();
+                line++;
+            }
             readch(br);
         }
         
         switch (peek) {
-            case '!':
-                peek = ' ';
-                return Token.not;
-             case '(':
-                peek = ' ';
-                return Token.lpt;
-             case ')':
-                peek = ' ';
-                return Token.rpt;
-             case '[':
-                peek = ' ';
-                return Token.lpq;
-             case ']':
-                peek = ' ';
-                return Token.rpq;
-             case '{':
-                peek = ' ';
-                return Token.lpg;
-             case '}':
-                peek = ' ';
-                return Token.rpg;
-             case '+':
-                peek = ' ';
-                return Token.plus;
-	     case '-':
-                peek = ' ';
-                return Token.minus;
-	     case '_':
-		 peek = ' ';
-		 return Token.underscore;
-             case '*':
-                peek = ' ';
-                return Token.mult;
-             case '/':
-		 readch(br);
-		 if(peek=='*'){//start the comment.
-		     boolean comment=true;
-		     while(comment){
-			 readch(br);
-			 if (peek == '*') {
-			     readch(br);
-			     if (peek == '/') {
-				 comment=false;
-				 return lexical_scan(br);
-			     }
-			 }
-		     }
-		 }else if(peek=='/'){//start commentline  
-		     while(peek!='\n'){
-			 readch(br);
-		     }
-		     return lexical_scan(br);
-		 }else{
-		     return Token.div;
-		 }
-             case ';':
-                peek = ' ';
-                return Token.semicolon;
-             case ',':
-                peek = ' ';
-                return Token.comma;
+        case '!':
+            peek = ' ';
+            return Token.not;
+        case '(':
+            peek = ' ';
+            return Token.lpt;
+        case ')':
+            peek = ' ';
+            return Token.rpt;
+        case '[':
+            peek = ' ';
+            return Token.lpq;
+        case ']':
+            peek = ' ';
+            return Token.rpq;
+        case '{':
+            peek = ' ';
+            return Token.lpg;
+        case '}':
+            peek = ' ';
+            return Token.rpg;
+        case '+':
+            peek = ' ';
+            return Token.plus;
+        case '-':
+            peek = ' ';
+            return Token.minus;
+        case '_':
+            peek = ' ';
+            return Token.underscore;
+        case '*':
+            peek = ' ';
+            return Token.mult;
+        case '/':
+            readch(br);
+            if(peek=='*'){//start the comment.
+                boolean comment=true;
+                while(comment){
+                    readch(br);
+                    if (peek == '*') {
+                        readch(br);
+                        if (peek == '/') {
+                            comment=false;
+                            return lexical_scan(br);
+                        }
+                    }
+                }
+            }else if(peek=='/'){//start commentline
+                while(peek!='\n'){
+                    readch(br);
+                }
+                return lexical_scan(br);
+            }else{
+                return Token.div;
+            }
+        case ';':
+            peek = ' ';
+            return Token.semicolon;
+        case ',':
+            peek = ' ';
+            return Token.comma;
 
 
-            case '&':
-                readch(br);
-                if (peek == '&') {
-                    peek = ' ';
-                    return Word.and;
-                } else {
-                    System.err.println("Erroneous character"
-                            + " after & : "  + peek );
-                    return null;
+        case '&':
+            readch(br);
+            if (peek == '&') {
+                peek = ' ';
+                return Word.and;
+            } else {
+                System.err.println("Erroneous character"
+                                   + " after & : "  + peek );
+                return null;
+            }
+
+            // ... gestire i casi di || < > <= >= == <> ... //
+        case '|':
+            readch(br);
+            if (peek == '|') {
+                peek = ' ';
+                return Word.or;
+            } else {
+                System.err.println("Erroneous character after | : "  + peek );
+                return null;
+            }
+
+        case '<':
+            readch(br);
+            if (peek == '=') {
+                peek = ' ';
+                return Word.le;
+            } else if(peek == '>'){
+                peek = ' ';
+                return Word.ne;
+            } else {
+                //System.err.println("Erroneous character after < : "  + peek );
+                return Word.lt;
+            }
+
+        case '>':
+            readch(br);
+            if (peek == '=') {
+                peek = ' ';
+                return Word.ge;
+            } else {
+                //System.err.println("Erroneous character after > : "  + peek );
+                return Word.gt;
+            }
+
+        case '=':
+            readch(br);
+            if (peek == '=') {
+                peek = ' ';
+                return Word.eq;
+            } else {
+                System.err.println("Erroneous character"
+                                   + " after = : "  + peek );
+                return null;
+            }
+
+
+        case (char)-1:
+            return new Token(Tag.EOF);
+
+        default:
+            String word = "";
+            if (Character.isLetter(peek)) {
+                // ... gestire il caso degli identificatori e delle parole chiave //
+                do { //complete the word with the other characters
+                    word+=peek;
+                    readch(br);
+                }while(Character.isLetter(peek) || Character.isDigit(peek) || peek=='_');
+                switch(word){
+                case "assign":
+                    return Word.assign;
+                case "to":
+                    return Word.to;
+                case "conditional":
+                    return Word.conditional;
+                case "option":
+                    return Word.option;
+                case "do":
+                    return Word.dotok;
+                case "else":
+                    return Word.elsetok;
+                case "while":
+                    return Word.whiletok;
+                case "begin":
+                    return Word.begin;
+                case "end":
+                    return Word.end;
+                case "print":
+                    return Word.print;
+                case "read":
+                    return Word.read;
+                default:
+                    return new Word(Tag.ID,word);
                 }
 
-	// ... gestire i casi di || < > <= >= == <> ... //
-            case '|':
-                readch(br);
-                if (peek == '|') {
-                    peek = ' ';
-                    return Word.or;
-                } else {
-                    System.err.println("Erroneous character after | : "  + peek );
-                    return null;
-                }
+            }else if(Character.isDigit(peek)) {
 
-            case '<':
-                readch(br);
-                if (peek == '=') {
-                    peek = ' ';
-                    return Word.le;
-		} else if(peek == '>'){
-		    peek = ' ';
-		    return Word.ne;
-                } else {
-                    //System.err.println("Erroneous character after < : "  + peek );
-                    return Word.lt;
-                }
-
-            case '>':
-                readch(br);
-                if (peek == '=') {
-                    peek = ' ';
-                    return Word.ge;
-                } else {
-                    //System.err.println("Erroneous character after > : "  + peek );
-                    return Word.gt;
-                }
-
-            case '=':
-                readch(br);
-                if (peek == '=') {
-                    peek = ' ';
-                    return Word.eq;
-                } else {
-                    System.err.println("Erroneous character"
-                            + " after = : "  + peek );
-                    return null;
-                }
-
-          
-            case (char)-1:
-                return new Token(Tag.EOF);
-
-            default:
-		String word = "";
-                if (Character.isLetter(peek)) {
-		    // ... gestire il caso degli identificatori e delle parole chiave //
-		    do { //complete the word with the other characters 
-			word+=peek;
-			readch(br);
-		    }while(Character.isLetter(peek) || Character.isDigit(peek) || peek=='_');
-		    switch(word){
-		    case "assign":
-			return Word.assign;
-		    case "to":
-			return Word.to;
-		    case "conditional":
-			return Word.conditional;
-		    case "option":
-			return Word.option;
-		    case "do":
-			return Word.dotok;
-		    case "else":
-			return Word.elsetok;
-		    case "while":
-			return Word.whiletok;
-		    case "begin":
-			return Word.begin;
-		    case "end":
-			return Word.end;
-		    case "print":
-			return Word.print;
-		    case "read":
-			return Word.read;
-		    default:
-			return new Word(Tag.ID,word);
-		    }
-		       
-			
-                }else if(Character.isDigit(peek)) {
-
-		    // ... gestire il caso dei numeri ... //
-		    do { //Complete the number with the next digits.
-			word+=peek;
-			readch(br);
-		    } while (Character.isDigit(peek));
-		    return new NumberTok(Integer.parseInt(word));
-
-                } else {
-                        System.err.println("Erroneous character: " 
-                                + peek );
-                        return null;
-                }
-         }
+                // ... gestire il caso dei numeri ... //
+                do { //Complete the number with the next digits.
+                    word+=peek;
+                    readch(br);
+                } while (Character.isDigit(peek));
+                return new NumberTok(Integer.parseInt(word));
+            } else {
+                System.err.println("Erroneous character: "
+                                   + peek );
+                return null;
+            }
+        }
     }
 		
     public static void main(String[] args) {
@@ -208,7 +206,7 @@ public class Lexer {
             Token tok;
             do {
                 tok = lex.lexical_scan(br);
-		System.out.print(tok+" ");
+                System.out.print(tok+" ");
             } while (tok.tag != Tag.EOF);
             br.close();
         } catch (IOException e) {e.printStackTrace();}    
--- Esercizio5/Translator.java
+++ Esercizio5/Translator.java
@@ -12,40 +12,40 @@ public class Translator { // Un Parser32 adattato.
     public Translator(Lexer l, BufferedReader br) {
         lex = l;
         pbr = br;
-	move();
+		move();
     }
 
     void move() {
-	look = lex.lexical_scan(pbr);
+		look = lex.lexical_scan(pbr);
 
-	if(look.tag == ';' || look.tag == -1)
-	    System.out.println(look);
-	else
-	    System.out.print(look+ " ");
-    }
+		if(look.tag == ';' || look.tag == -1)
+			System.out.println(look);
+		else
+			System.out.print(look+ " ");
+	}
 
     private void error(String s) {
-	throw new Error("\nnear line " + lex.line + ":"+ look +": " + s);
+		throw new Error("\nnear line " + lex.line + ":"+ look +": " + s);
     }
 
     private void match(int t) {
-	if (look.tag == t) {
-	    if (look.tag != Tag.EOF) move();
-	} else error("syntax error");
+		if (look.tag == t) {
+			if (look.tag != Tag.EOF) move();
+		} else error("syntax error");
     }
 
     private void prog(){//prog() -> statlist(lnext) EOF
 
-	if (look.tag == Tag.EOF) {
-	    error("file vuoto");
-	}
+		if (look.tag == Tag.EOF) {
+			error("file vuoto");
+		}
 
-    int proglabel = code.newLabel();
-	statlist(proglabel);
-	match(Tag.EOF);
+		int proglabel = code.newLabel();
+		statlist(proglabel);
+		match(Tag.EOF);
 
-	code.emit(OpCode.GOto,proglabel);
-    code.emitLabel(proglabel);
+		code.emit(OpCode.GOto,proglabel);
+		code.emitLabel(proglabel);
 
 
         try {
@@ -57,128 +57,126 @@ public class Translator { // Un Parser32 adattato.
     }
 
     private void statlist(int labelAttuale){
-	stat(labelAttuale);
-	int lnext = code.newLabel();
-	code.emit(OpCode.GOto,lnext);
-	code.emitLabel(lnext);
-	statlistp(lnext);
-    }
-
-    private void statlistp(int labelAttuale){
-	if(look.tag == ';'){
-		match(';');
 		stat(labelAttuale);
 		int lnext = code.newLabel();
 		code.emit(OpCode.GOto,lnext);
 		code.emitLabel(lnext);
 		statlistp(lnext);
-	}
+    }
+
+    private void statlistp(int labelAttuale){
+		if(look.tag == ';'){
+			match(';');
+			stat(labelAttuale);
+			int lnext = code.newLabel();
+			code.emit(OpCode.GOto,lnext);
+			code.emitLabel(lnext);
+			statlistp(lnext);
+		}
     }
 
     private void stat(int labelattuale){
-	switch (look.tag) {
-	case Tag.ASSIGN:
-	    match(Tag.ASSIGN);
-	    expr();
-	    match(Tag.TO);
-	    idlist(Tag.ASSIGN);
-	    break;
-	case Tag.PRINT:
-	    match(Tag.PRINT);
-	    match('[');
-	    exprlist(Tag.PRINT);
-	    match(']');
-	    break;
-	case Tag.READ:
-	    match(Tag.READ);
-	    match('[');
-	    idlist(Tag.READ);
-	    match(']');
-	    break;
-	case Tag.WHILE:
-	    match(Tag.WHILE);
-	    int endwhile   = code.newLabel();
-	    match('(');
-	    bexpr(endwhile);
-	    match(')');
-	    stat(labelattuale);
-	    code.emit(OpCode.GOto,labelattuale);
-	    code.emitLabel(endwhile);
-	    break;
-	case Tag.COND:
-		int lendCondition = code.newLabel();
-
-	    match(Tag.COND);
-	    match('[');
-	    optlist(lendCondition);
-	    match(']');
-	    switch (look.tag) {
-	    case Tag.ELSE:
-		match(Tag.ELSE);
-		stat(labelattuale);
-	    case Tag.END:
-		match(Tag.END);
-		code.emitLabel(lendCondition);
-		break;
-	    default:
-		error("not valid command");
-
-	    }
-	    break;
-	case '{':
-	    match('{');
-	    statlist(labelattuale);
-	    match('}');
-	    break;
-	default:
-	    error("not valid stat");
-	}
+		switch (look.tag) {
+		case Tag.ASSIGN:
+			match(Tag.ASSIGN);
+			expr();
+			match(Tag.TO);
+			idlist(Tag.ASSIGN);
+			break;
+		case Tag.PRINT:
+			match(Tag.PRINT);
+			match('[');
+			exprlist(Tag.PRINT);
+			match(']');
+			break;
+		case Tag.READ:
+			match(Tag.READ);
+			match('[');
+			idlist(Tag.READ);
+			match(']');
+			break;
+		case Tag.WHILE:
+			match(Tag.WHILE);
+			int endwhile   = code.newLabel();
+			match('(');
+			bexpr(endwhile);
+			match(')');
+			stat(labelattuale);
+			code.emit(OpCode.GOto,labelattuale);
+			code.emitLabel(endwhile);
+			break;
+		case Tag.COND:
+			int lendCondition = code.newLabel();
+			match(Tag.COND);
+			match('[');
+			optlist(lendCondition);
+			match(']');
+			switch (look.tag) {
+			case Tag.ELSE:
+				match(Tag.ELSE);
+				stat(labelattuale);
+			case Tag.END:
+				match(Tag.END);
+				code.emitLabel(lendCondition);
+				break;
+			default:
+				error("not valid command");
+			}
+			break;
+		case '{':
+			match('{');
+			statlist(labelattuale);
+			match('}');
+			break;
+		default:
+			error("not valid stat");
+		}
     }
     private void idlist(int op){
-	if(look.tag == Tag.ID){
-	    int address = st.lookupAddress(look);
-	    if (address == -1) {
-		st.insert(((Word)look).lexeme,count);
-		address = count++;
-	    }
+		if(look.tag == Tag.ID){
+			int address = st.lookupAddress(look);
+			if (address == -1) {
+				st.insert(((Word)look).lexeme,count);
+				address = count++;
+			}
 
-	    if (op == Tag.READ) {
-		code.emit(OpCode.invokestatic,0); // invokestatic 0 = read
-	    }
-	    code.emit(OpCode.istore,address);
-		match(Tag.ID);
-	    idlistp(op);
-	}else{
-	    error("no identifier was found.");
-	}
+			if (op == Tag.READ) {
+				code.emit(OpCode.invokestatic,0); // invokestatic 0 = read
+			}
+			code.emit(OpCode.istore,address);
+			match(Tag.ID);
+			idlistp(op);
+		}else{
+			error("no identifier was found.");
+		}
     }
+
     private void idlistp(int op){
-	if(look.tag == ','){
-	    match(',');
-	    if(look.tag == Tag.ID){
-		int address = st.lookupAddress(look);
-		if (address == -1) {
-		    st.insert(((Word)look).lexeme,count);
-		    address = count++;
-		}
+		if(look.tag == ','){
+			match(',');
+			if(look.tag == Tag.ID){
+				int address = st.lookupAddress(look);
+				if (address == -1) {
+					st.insert(((Word)look).lexeme,count);
+					address = count++;
+				}
 
-		if (op == Tag.READ) {
-		    code.emit(OpCode.invokestatic,0); // invokestatic 0 = read
+				if (op == Tag.READ) {
+					code.emit(OpCode.invokestatic,0); // invokestatic 0 = read
+				}
+				code.emit(OpCode.istore,address);
+				match(Tag.ID);
+				idlistp(op);
+			}else{
+				error("no identifier was found.");
+			}
 		}
-		code.emit(OpCode.istore,address);
-		match(Tag.ID);
-		idlistp(op);
-	    }else{
-		error("no identifier was found.");
-	    }
-
-	}
     }
 
     private void optlist(int lendCondition){
 		int lnext = code.newLabel();
-	optitem(lnext,lendCondition);
-	optlistp(lendCondition);
+		optitem(lnext,lendCondition);
+		optlistp(lendCondition);
     }
 
 	private void optlistp(int lendCondition){
@@ -190,152 +188,150 @@ public class Translator { // Un Parser32 adattato.
     }
 
     private void optitem(int lnext, int lendCondition){
-	int ltrue = code.newLabel();
-	match(Tag.OPTION);
-	match('(');
-	bexpr(ltrue);
-	match(')');
-	code.emit(OpCode.GOto,lnext);
-	code.emitLabel(ltrue);
-	match(Tag.DO);
-	stat(lnext);
-	code.emit(OpCode.GOto,lendCondition);
-	code.emitLabel(lnext);
+		int ltrue = code.newLabel();
+		match(Tag.OPTION);
+		match('(');
+		bexpr(ltrue);
+		match(')');
+		code.emit(OpCode.GOto,lnext);
+		code.emitLabel(ltrue);
+		match(Tag.DO);
+		stat(lnext);
+		code.emit(OpCode.GOto,lendCondition);
+		code.emitLabel(lnext);
     }
 
     private void bexpr(int truelabel){
-	switch (look.tag) {
-	case Tag.RELOP:
-	    String relatinaloperator = ((Word)look).lexeme;
-	    match(Tag.RELOP);
-	    expr();
-	    expr();
-	    switch (relatinaloperator) {
-	    case ">":
-		code.emit(OpCode.if_icmpgt,truelabel);
-		break;
-	    case "<":
-		code.emit(OpCode.if_icmplt,truelabel);
-		break;
-		
-	    case "<=":
-		code.emit(OpCode.if_icmple,truelabel);
-		break;
-	    case ">=":
-		code.emit(OpCode.if_icmpge,truelabel);
-		break;
-	    case "==":
-		code.emit(OpCode.if_icmpeq,truelabel);
-		break;
-	    case "<>":
-		code.emit(OpCode.if_icmpne,truelabel);
-		break;
-	    }
+		switch (look.tag) {
+		case Tag.RELOP:
+			String relatinaloperator = ((Word)look).lexeme;
+			match(Tag.RELOP);
+			expr();
+			expr();
+			switch (relatinaloperator) {
+			case ">":
+				code.emit(OpCode.if_icmpgt,truelabel);
+				break;
+			case "<":
+				code.emit(OpCode.if_icmplt,truelabel);
+				break;
+			case "<=":
+				code.emit(OpCode.if_icmple,truelabel);
+				break;
+			case ">=":
+				code.emit(OpCode.if_icmpge,truelabel);
+				break;
+			case "==":
+				code.emit(OpCode.if_icmpeq,truelabel);
+				break;
+			case "<>":
+				code.emit(OpCode.if_icmpne,truelabel);
+				break;
+			}
 
-	    break;
-	case '!':
-	    match('!');
-	    //bexpr();
-	    code.emit(OpCode.ineg, truelabel);
-	    break;
-	case Tag.AND:
-	    match(Tag.AND);
-	    expr();
-	    expr();
-	    code.emit(OpCode.iand, truelabel);
-	    break;
-	case Tag.OR:
-	    match(Tag.OR);
-	    expr();
-	    expr();
-	    code.emit(OpCode.ior, truelabel);
-	    break;
-	default:
-	    error("is not a relational operator");
-	}
+			break;
+		case '!':
+			match('!');
+			//bexpr();
+			code.emit(OpCode.ineg, truelabel);
+			break;
+		case Tag.AND:
+			match(Tag.AND);
+			expr();
+			expr();
+			code.emit(OpCode.iand, truelabel);
+			break;
+		case Tag.OR:
+			match(Tag.OR);
+			expr();
+			expr();
+			code.emit(OpCode.ior, truelabel);
+			break;
+		default:
+			error("is not a relational operator");
+		}
     }
 
     private void expr(){
-	switch (look.tag) {
-	case '+':
-	    match('+');
-	    match('(');
-	    exprlist('+');
-	    match(')');
-	    break;
+		switch (look.tag) {
+		case '+':
+			match('+');
+			match('(');
+			exprlist('+');
+			match(')');
+			break;
 
-	case '*':
-	    match('*');
-	    match('(');
-	    exprlist('*');
-	    match(')');
-	    break;
-	case '-':
-	    match('-');
-	    expr();
-	    expr();
-	    code.emit(OpCode.isub);
-	    break;
-	case '/':
-	    match('/');
-	    expr();
-	    expr();
-	    code.emit(OpCode.idiv);
-	    break;
-	case Tag.NUM:
-	    /*devo fare il cast a NumberTok perche java non sa che il look e'
+		case '*':
+			match('*');
+			match('(');
+			exprlist('*');
+			match(')');
+			break;
+		case '-':
+			match('-');
+			expr();
+			expr();
+			code.emit(OpCode.isub);
+			break;
+		case '/':
+			match('/');
+			expr();
+			expr();
+			code.emit(OpCode.idiv);
+			break;
+		case Tag.NUM:
+			/*devo fare il cast a NumberTok perche java non sa che il look e'
 	     *un NumberTok e non un semplice Token.
 	    */
-	    int num = ((NumberTok)look).num; //(numberTok)look dice al look di comportarsi come NumberTok
-	    code.emit(OpCode.ldc,num);
-	    match(Tag.NUM);
-	    break;
-	case Tag.ID:
-	    /*Questo e' l'unico momento di lettura dei SymbolTable*/
-	    int address = st.lookupAddress(look);
+			int num = ((NumberTok)look).num; //(numberTok)look dice al look di comportarsi come NumberTok
+			code.emit(OpCode.ldc,num);
+			match(Tag.NUM);
+			break;
+		case Tag.ID:
+			/*Questo e' l'unico momento di lettura dei SymbolTable*/
+			int address = st.lookupAddress(look);
 
-	    if (address != -1) {
-		code.emit(OpCode.iload, address);
-	    }else{
-		error("identifier not found.");
-	    }
-	    match(Tag.ID);
-	    break;
-	default:
-	    error("Invalid Expresion");
-	}
-
+			if (address != -1) {
+				code.emit(OpCode.iload, address);
+			}else{
+				error("identifier not found.");
+			}
+			match(Tag.ID);
+			break;
+		default:
+			error("Invalid Expresion");
+		}
     }
 
     private void exprlist(int op){
-	expr();
-	if(op == Tag.PRINT){
-	    /*invokestatic 1 significa print, altrimente sarebbe READ(non e'
+		expr();
+		if(op == Tag.PRINT){
+			/*invokestatic 1 significa print, altrimente sarebbe READ(non e'
 	    specificato nel progetto ma possiamo dire che sarebbe 0 in quel caso)*/
-	    code.emit(OpCode.invokestatic,1);//nel caso del print dobbiamo usarlo per ogni espressione perche e' un'operazione unitaria e non binaria 
-	}
-	exprlistp(op);
+			code.emit(OpCode.invokestatic,1);//nel caso del print dobbiamo usarlo per ogni espressione perche e' un'operazione unitaria e non binaria
+		}
+		exprlistp(op);
     }
 
     private void exprlistp(int op){
-	if(look.tag == ','){
-	    match(',');
-	    expr();
-	    switch (op) {
-	    case '+':
-		code.emit(OpCode.iadd);
-		break;
-	    case '*':
-		code.emit(OpCode.imul);
-		break;
-	    case Tag.PRINT:
-		code.emit(OpCode.invokestatic,1); // invokestatic 1 = print
-		break;
-	    default:
-		error("invalid Operator");
-	    }
-	    exprlistp(op);
-	}
+		if(look.tag == ','){
+			match(',');
+			expr();
+			switch (op) {
+			case '+':
+				code.emit(OpCode.iadd);
+				break;
+			case '*':
+				code.emit(OpCode.imul);
+				break;
+			case Tag.PRINT:
+				code.emit(OpCode.invokestatic,1); // invokestatic 1 = print
+				break;
+			default:
+				error("invalid Operator");
+			}
+			exprlistp(op);
+		}
     }
 
     public static void main(String[] args) {
